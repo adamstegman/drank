8a502445be098c05293f7c593eb0c23b7afb4aee
Adam Stegman <astegman@gmail.com>
2010-09-26 23:14:54 -0500

    Drink model has some meat to it now. Added a relationship to Person. Added "today" scope.

--- app/models/drink.rb
+++ app/models/drink.rb
@@ -9,4 +9,25 @@
 # == Relationships
 # Person:: The Person who made this drink.
 class Drink < ActiveRecord::Base
+  # == Constants
+  NEW_DAY_HOUR = 4
+  
+  # == Relationships
+  belongs_to :person
+  
+  # == Validations
+  validates_presence_of :amount, :person
+  
+  # == Class Methods
+  scope :today, -> do
+    now = Time.now
+    # The cutoff is the hour of today when we consider "today" started
+    cutoff = Time.new(now.year, now.month, now.day, Drink::NEW_DAY_HOUR, 0, 0)
+    # Get yesterday's drinks if it's not yet what we consider a new day
+    if now.hour < NEW_DAY_HOUR
+      cutoff = Time.new(now.year, now.month, now.day - 1, Drink::NEW_DAY_HOUR, 0, 0)
+    end
+    
+    where("drinks.created_at >= ?", cutoff)
+  end
 end
--- test/unit/drink_test.rb
+++ test/unit/drink_test.rb
@@ -1,8 +1,82 @@
+# coding: UTF-8
+
 require 'test_helper'
 
 class DrinkTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  # amount tests
+  test "should require amount" do
+    assert Drink.new.requires?(:amount)
+  end
+  
+  # person tests
+  test "should required person" do
+    assert Drink.new.requires?(:person)
+  end
+  
+  # today scope tests
+  test "should return drinks from today if now is on or after new day hour" do
+    Person.find_or_create_by_name('Adam')
+    today = Date.today
+    
+    # mock Time.now
+    Time.instance_eval do
+      def today=(today)
+        @today = today
+      end
+      
+      alias :old_now :now
+      def now
+        Time.new(@today.year, @today.month, @today.day, Drink::NEW_DAY_HOUR, 0, 0)
+      end
+    end
+    Time.today = today
+    
+    y = Drink.new(:amount => 1, :person_id => Person.first.id,
+                  :created_at => Time.new(today.year, today.month, today.day, Drink::NEW_DAY_HOUR - 1, 59, 59))
+    y.save!
+    t = Drink.new(:amount => 1, :person_id => Person.first.id,
+                  :created_at => Time.new(today.year, today.month, today.day, Drink::NEW_DAY_HOUR, 0, 0))
+    t.save!
+    assert_equal [t], Drink.today
+    
+    # unmock Time.now
+    Time.instance_eval do
+      alias :now :old_now
+      undef :old_now
+      undef :today=
+    end
+  end
+  
+  test "should return drinks from yesterday if now is before new day hour" do
+    Person.find_or_create_by_name('Adam')
+    today = Date.today
+    
+    # mock Time.now
+    Time.instance_eval do
+      def today=(today)
+        @today = today
+      end
+      
+      alias :old_now :now
+      def now
+        Time.new(@today.year, @today.month, @today.day, Drink::NEW_DAY_HOUR - 1, 59, 59)
+      end
+    end
+    Time.today = today
+    
+    y = Drink.new(:amount => 1, :person_id => Person.first.id,
+                  :created_at => Time.new(today.year, today.month, today.day - 1, Drink::NEW_DAY_HOUR - 1, 59, 59))
+    y.save!
+    t = Drink.new(:amount => 1, :person_id => Person.first.id,
+                  :created_at => Time.new(today.year, today.month, today.day - 1, Drink::NEW_DAY_HOUR, 0, 0))
+    t.save!
+    assert_equal [t], Drink.today
+    
+    # unmock Time.now
+    Time.instance_eval do
+      alias :now :old_now
+      undef :old_now
+      undef :today=
+    end
   end
 end
