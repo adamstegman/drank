71a8b36e3ad7a2e97e279a9bbe6d1cdd26cdca7c
Adam Stegman <me@adamstegman.com>
2017-05-29 13:30:19 -0700

    Simple drank display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+export class Drinker {
+  id: number;
+  name: string;
+  drank: number;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Water Wars';
+  drinkers: Drinker[] = [
+    {id: 1, name: "Aubrey", drank: 0},
+    {id: 2, name: "Adam", drank: 0},
+  ];
 }
