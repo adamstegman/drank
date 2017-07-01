a1ed3769cd58c8a8e7b25ee5cc3168a7eb33151d
Adam Stegman <me@adamstegman.com>
2017-07-01 16:37:43 -0700

    Automatically reload drink totals at midnight

--- src/app/drinks.service.ts
+++ src/app/drinks.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import * as moment from 'moment';
@@ -10,16 +11,22 @@ import { Drink } from './drink';
 
 @Injectable()
 export class DrinksService {
+  startDrinksAtTimestamp: BehaviorSubject<string>;
+  endDrinksAtTimestamp: BehaviorSubject<string>;
   drinks: FirebaseListObservable<Drink[]>;
 
   constructor(private db: AngularFireDatabase) {
+    this.startDrinksAtTimestamp = new BehaviorSubject(moment().startOf('day').utc().toJSON());
+    this.endDrinksAtTimestamp = new BehaviorSubject(moment().endOf('day').utc().toJSON());
     this.drinks = db.list('/drinks', {
       query: {
         orderByChild: 'time',
-        startAt: this.beginningOfDayTimestamp(),
-        endAt: this.endOfDayTimestamp()
+        startAt: this.startDrinksAtTimestamp,
+        endAt: this.endDrinksAtTimestamp
       }
     });
+
+    this.updateDrinkTimestamps();
   }
 
   addDrink(drinker: Drinker, drank: number): void {
@@ -36,11 +43,13 @@ export class DrinksService {
       map(drinks => drinks.reduce((acc, drink) => acc + drink.amount, 0));
   }
 
-  beginningOfDayTimestamp(): string {
-    return moment().startOf('day').utc().toJSON();
-  }
+  updateDrinkTimestamps(): void {
+    const beginningOfDay = moment().startOf('day');
+    this.startDrinksAtTimestamp.next(beginningOfDay.clone().utc().toJSON());
+    const endOfDay = beginningOfDay.clone().endOf('day');
+    this.endDrinksAtTimestamp.next(endOfDay.clone().utc().toJSON());
 
-  endOfDayTimestamp(): string {
-    return moment().endOf('day').utc().toJSON();
-  }
+    const msUntilTomorrow = beginningOfDay.clone().add(1, 'day').diff(moment.now());
+    setTimeout(this.updateDrinkTimestamps.bind(this), msUntilTomorrow);
+  };
 }
